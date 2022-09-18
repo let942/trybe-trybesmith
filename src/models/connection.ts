6d4c077aea9e32b14e651147b5c790d13b4847fd
soficia <let942@gmail.com>
2022-09-18 16:53:21 -0300

    config: atualiza configuração

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,9 +4,9 @@ import mysql from 'mysql2/promise';
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
 });
 
 export default connection;
