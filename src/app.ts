97bde77d7a83fd873ed58aaa53a1c871d7379960
soficia <let942@gmail.com>
2022-09-19 11:49:49 -0300

    feat: cria validação para os produtos

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,13 @@ import usersRoute from './routes/users.routes';
 import ordersRoute from './routes/orders.routes';
 import loginRoute from './routes/login.routes';
 import bodyLoginValidation from './middlewares/bodyLoginValidation';
+import { productValidationName, productValidationAmount } from './middlewares/productValidation';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/products', productsRoute);
+app.use('/products', productValidationName, productValidationAmount, productsRoute);
 app.use('/users', usersRoute);
 app.use('/orders', ordersRoute);
 app.use('/login', bodyLoginValidation, loginRoute);
