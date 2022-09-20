8ed9ecbe07931d35d1294b9a959bdd1176579f13
soficia <let942@gmail.com>
2022-09-19 21:12:39 -0300

    refactor: corrige rotas de products

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,12 @@ import usersRoute from './routes/users.routes';
 import ordersRoute from './routes/orders.routes';
 import loginRoute from './routes/login.routes';
 import bodyLoginValidation from './middlewares/bodyLoginValidation';
-import { productValidationName, productValidationAmount } from './middlewares/productValidation';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/products', productValidationName, productValidationAmount, productsRoute);
+app.use('/products', productsRoute);
 app.use('/users', usersRoute);
 app.use('/orders', ordersRoute);
 app.use('/login', bodyLoginValidation, loginRoute);
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import ProductsController from '../controllers/products.controller';
+import { productValidationName, productValidationAmount } from '../middlewares/productValidation';
+
 
 const productsRoute = express.Router();
 
-productsRoute.post('/', ProductsController.create);
+productsRoute.post('/', productValidationName, productValidationAmount, ProductsController.create);
 productsRoute.get('/', ProductsController.getAll);
 
 export default productsRoute;
